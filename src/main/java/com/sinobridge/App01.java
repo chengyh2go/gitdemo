3e08c47db3d6f373201f9211e5102fe826e8a370
chengyh2go <chengyh2go@gmail.com>
2022-02-15 13:01:43 +0800

    release-1.0

--- src/main/java/com/sinobridge/App01.java
+++ src/main/java/com/sinobridge/App01.java
@@ -3,5 +3,7 @@ package com.sinobridge;
 public class App01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("release-1.0");
     }
 }
